b74e60a34cfe0013fc2b1c6e70983dd014ad1363
hachibu <raymondsohn@gmail.com>
2021-01-13 07:56:48 -0800

    Refactor to utilize React Hooks

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -1,91 +1,88 @@
-class App extends React.Component {
-  state = {
-    details: [
-      {
-        title: 'What is this?',
-        description: <span>
-          This is an experimental password generator written in ReactJS that uses a video of you to
-          generate a random password. You can find more details on my <a className="text-white font-weight-bold"
-                                                                         style={{textDecoration: 'underline'}}
-                                                                         href="https://hachibu.net/posts/2019/be-your-own-password-generator/"
-                                                                         target="_blank">blog</a>.
-        </span>,
-        open: true
-      },
-      {
-        title: 'How does this work?',
-        description: <span>
-          It records a short video of you and then transforms that raw video
-          data into an array of 8-bit unsigned integers. Then it randomly
-          selects an integer from that array and transforms it into its
-          corresponding UTF-16 character. If that character is a lowercase
-          letter, uppercase letter, digit or special character it will be used
-          for your password. The process of selecting a random character
-          continues until the password is long enough.
-        </span>,
-        open: false
-      },
-      {
-        title: 'Is the video saved anywhere?',
-        description: <span>
-          The video is only stored temporarily in your browser for playback
-          purposes. The video will dissapear as soon as you close or refresh
-          this page.
-        </span>,
-        open: false
-      },
-      {
-        title: 'Is it safe to use these passwords?',
-        description: <span>
-          I would strongly suggest that you do not use these passwords.
-          I haven't run any proper entropy tests on the generated passwords.
-          So, I can't confirm or deny the actual strength of these passwords.
-          Please keep using your password manager to generate passwords.
-        </span>,
-        open: false
-      },
-    ],
-    password: '',
-    showPassword: false
+function App() {
+  let details = [
+    {
+      title: 'What is this?',
+      description: <span>
+        This is an experimental password generator written in ReactJS that uses a video of you to
+        generate a random password. You can find more details on my <a className="text-white font-weight-bold"
+                                                                       style={{textDecoration: 'underline'}}
+                                                                       href="https://hachibu.net/posts/2019/be-your-own-password-generator/"
+                                                                       target="_blank">blog</a>.
+      </span>,
+      open: true
+    },
+    {
+      title: 'How does this work?',
+      description: <span>
+        It records a short video of you and then transforms that raw video
+        data into an array of 8-bit unsigned integers. Then it randomly
+        selects an integer from that array and transforms it into its
+        corresponding UTF-16 character. If that character is a lowercase
+        letter, uppercase letter, digit or special character it will be used
+        for your password. The process of selecting a random character
+        continues until the password is long enough.
+      </span>,
+      open: false
+    },
+    {
+      title: 'Is the video saved anywhere?',
+      description: <span>
+        The video is only stored temporarily in your browser for playback
+        purposes. The video will dissapear as soon as you close or refresh
+        this page.
+      </span>,
+      open: false
+    },
+    {
+      title: 'Is it safe to use these passwords?',
+      description: <span>
+        I would strongly suggest that you do not use these passwords.
+        I haven't run any proper entropy tests on the generated passwords.
+        So, I can't confirm or deny the actual strength of these passwords.
+        Please keep using your password manager to generate passwords.
+      </span>,
+      open: false
+    },
+  ];
+  let [password, setPassword] = React.useState('');
+  let [showPassword, setShowPassword] = React.useState(false);
+
+  function handleRecordingStart() {
+    setShowPassword(false);
   }
 
-  handleRecordingStart = () => {
-    this.setState({ showPassword: false });
+  function handleRecordingEnd(password) {
+    setPassword(password);
+    setShowPassword(true);
   }
 
-  handleRecordingEnd = (password) => {
-    this.setState({ password, showPassword: true });
-  }
+  return (
+    <div className="container">
+      <div className="row mb-4 pl-3 pr-3">
+        <input id="password"
+               defaultValue={password}
+               className={`w-100 p-4 alert alert-success form-control ${showPassword ? 'd-block': 'd-none'}`}
+               type="text"
+               style={{fontSize: '30px'}} />
+      </div>
 
-  render() {
-    return (
-      <div className="container">
-        <div className="row mb-4 pl-3 pr-3">
-          <input id="password"
-                 defaultValue={this.state.password}
-                 className={`w-100 p-4 alert alert-success form-control ${this.state.showPassword ? 'd-block': 'd-none'}`}
-                 type="text"
-                 style={{fontSize: '30px'}} />
+      <div className="row">
+        <div className="col mb-4">
+          <div className="text-white mb-4 h-100">
+            <h1 className="mb-5">Be Your Own Password Generator</h1>
+            <DetailsComponent details={details} />
+          </div>
         </div>
 
-        <div className="row">
-          <div className="col mb-4">
-            <div className="text-white mb-4 h-100">
-              <h1 className="mb-5">Be Your Own Password Generator</h1>
-              <DetailsComponent details={this.state.details} />
-            </div>
-          </div>
-
-          <div className="col">
-            <div className="bg-white rounded p-4 mb-4">
-              <VideoRecordingComponent onRecordingStart={this.handleRecordingStart}
-                                       onRecordingEnd={this.handleRecordingEnd} />
-            </div>
+        <div className="col">
+          <div className="bg-white rounded p-4 mb-4">
+            <VideoRecordingComponent onRecordingStart={handleRecordingStart}
+                                     onRecordingEnd={handleRecordingEnd} />
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 function DetailsComponent(props) {
@@ -108,11 +105,14 @@ DetailsComponent.propTypes = {
       description: PropTypes.element.isRequired,
       open: PropTypes.bool.isRequired,
     })
-  ),
+  ).isRequired
 };
 
 function VideoRecordingComponent(props) {
-  const recordVideo = (videoLength) => {
+  let [isRecording, setRecording] = React.useState(false);
+  let videoRef = React.useRef(null);
+
+  function recordVideo(videoLength) {
     return new Promise((resolve, reject) => {
       let mediaData = [];
       let userMediaOptions = {
@@ -127,7 +127,7 @@ function VideoRecordingComponent(props) {
         recorder.addEventListener('stop', () => {
           let buffer = new Blob(mediaData);
           let videoSrc = window.URL.createObjectURL(buffer);
-          document.getElementById('video').src = videoSrc;
+          videoRef.current.src = videoSrc;
         });
         recorder.start();
         setTimeout(() => {
@@ -138,9 +138,9 @@ function VideoRecordingComponent(props) {
         }, videoLength * 1000);
       }, reject);
     });
-  };
+  }
 
-  const randIndex = (length) => {
+  function randIndex(length) {
     let min = (-length >>> 0) % length;
     let randNum = new Uint32Array(1);
     let x;
@@ -148,9 +148,9 @@ function VideoRecordingComponent(props) {
       x = window.crypto.getRandomValues(randNum);
     } while (x < min);
     return x % length;
-  };
+  }
 
-  const generatePassword = () => {
+  function generatePassword() {
     let formData = new FormData(document.getElementById('options'));
     let videoLength = parseInt(formData.get('video-length'), 10);
     let passwordLength = parseInt(formData.get('password-length'), 10);
@@ -175,18 +175,16 @@ function VideoRecordingComponent(props) {
         }, reject);
       }, reject);
     });
-  };
+  }
 
-  const [isRecording, setRecording] = React.useState(false);
-
-  const startRecording = () => {
+  function startRecording() {
     setRecording(true);
     props.onRecordingStart(password);
     generatePassword().then(password => {
       setRecording(false);
       props.onRecordingEnd(password);
     }, alert);
-  };
+  }
 
   return (
     <form id="options">
@@ -215,6 +213,7 @@ function VideoRecordingComponent(props) {
         </button>
       </div>
       <video controls
+             ref={videoRef}
              id="video"
              className="rounded border bg-light w-100 d-block"
              style={{ height: '300px' }}></video>
